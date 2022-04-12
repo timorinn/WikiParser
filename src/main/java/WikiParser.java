dd97715eea2c8a68bdac3685755f549fd1c2c4d7
timorinn <timorinn@yandex.ru>
2022-04-12 06:21:07 +0300

    init commit add my own Json Library

---
+++ src/main/java/WikiParser.java
@@ -0,0 +1,35 @@
+
+import jsonlib.JsonCommon;
+import jsonlib.JsonParser;
+
+public class WikiParser {
+	public static void main(String[] args) {
+//		try {
+//			String url = "https://wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=IKEA&redirects=1&formatversion=2&exlimit=1&exintro=1&explaintext=1";
+//
+//			URL obj = new URL(url);
+//			HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+//
+//			connection.setRequestMethod("GET");
+//
+//			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//			String inputLine;
+//			StringBuffer response = new StringBuffer();
+//
+//			while ((inputLine = in.readLine()) != null) {
+//				response.append(inputLine);
+//			}
+//			in.close();
+//
+//			System.out.println(response.toString());
+//
+//			jsonlib.JsonObject jsonObject = jsonlib.JsonParser.getJsonObject(response.toString());
+//
+//			System.out.println(jsonObject.getValue("query/pages/[0]/pageid"));
+//
+//			System.out.println("ALL FINE");
+//		} catch (Exception e) {
+//			System.out.println("EXCEPTION ! ! ! " + e.toString());
+//		}
+	}
+}
